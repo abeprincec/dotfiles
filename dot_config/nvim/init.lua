413e89cbb18805869ad58ef73b316efc5bcd8f07
Abraham Prince <abeprincec@gmail.com>
2024-01-16 23:24:23 -0500

    add nvim setup telescope

---
+++ dot_config/nvim/init.lua
@@ -0,0 +1,2 @@
+require('abraham')
+print('hello')
---
+++ dot_config/nvim/lua/abraham/init.lua
@@ -0,0 +1,28 @@
+require("abraham.remap")
+print('hello from the abraham')
+
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+	{
+		'nvim-telescope/telescope.nvim', tag = '0.1.5',
+		-- or                              , branch = '0.1.x',
+		dependencies = { 'nvim-lua/plenary.nvim' }
+	},
+
+	{ 'rose-pine/neovim', name = 'rose-pine' }
+
+})
+
